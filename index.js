24bbda50496287627a680cd06089e0ccf3d27d75
venugopal <venugopalcj1990@gmail.com>
2020-11-10 19:26:08 +0100

    Added get and post request

--- index.js
+++ index.js
@@ -1,78 +1,3 @@
-/* const express = require("express");
-const cors = require("cors");
-const mysql = require("mysql");
-const bodyParser = require('body-parser');
-
-const app = express();
-
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'test'
-});
-
-connection.connect(err => {
-    if (err) {
-        return err;
-    }
-});
-
-app.use(cors());
-
-app.get('/', (req, res) => {
-    res.send('Hello Welcome')
-});
-
-app.get('/selecttable', (req, res) => {
-    const SQL = 'SELECT * FROM peraons';
-    connection.query(SQL, (err, result) => {
-        if (err) {
-            throw err;
-        }
-        console.log(result)
-        res.send(JSON.stringify(result))
-    });
-});
-
-app.post('/peraons', (req, res) => {
-    const { id, first_name, last_name, age } = req.query;
-    const insert_data = `INSERT INTO peraons(person_id, first_name, last_name, age) VALUES (${id}, '${first_name}', '${last_name}', ${age})`;
-    //const insert_data = `INSERT INTO peraons(person_id, first_name, last_name, age) VALUES (7, 'what', 'is', 47)`;
-    connection.query(insert_data, (err, result) => {
-        if (err) {
-            throw err;
-        }
-        console.log(result)
-        return res.send(JSON.stringify(result))
-    });
-});
-
-/* let pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'test'
-});
-
-
-pool.getConnection(function (err, connection) {
-    connection.query('SELECT something FROM sometable', function (err, rows) {
-
-        console.log(pool._freeConnections.indexOf(connection)); // -1
-
-        connection.release();
-
-        console.log(pool._freeConnections.indexOf(connection)); // 0
-
-    });
-});
-
-
-app.listen(4000, () => {
-    console.log("product is listening at 4000")
-}); */
-
 // New code
 var express = require('express');
 var app = express();
@@ -87,7 +12,6 @@ app.use(express.static('public'));
 
 
 var connection = mysql.createConnection({
-
     host: 'localhost',
     user: 'root',
     password: 'root',
@@ -106,7 +30,14 @@ app.use(cors());
 
 app.get('/', function (req, res) {
 
-    res.sendFile(__dirname + '/index.html');
+    //res.sendFile(__dirname + '/index.html');
+    const SQL = 'SELECT * FROM person';
+    connection.query(SQL, (err, result) => {
+        if (err) {
+            throw err;
+        }
+        res.send(JSON.stringify(result))
+    });
 
 });
 
@@ -124,7 +55,7 @@ app.all("/*", function (req, res, next) {
 app.post('/data', function (req, res) {
     console.log(req);
 
-    var sql = "INSERT INTO `Persons`(`name`,`age`) VALUES ('" + req.body.name + "','" + req.body.age + "')";
+    /* var sql = "INSERT INTO `Persons`(`name`,`age`) VALUES ('" + req.body.name + "','" + req.body.age + "')";
     connection.query(sql, function (err, result) {
         if (err) throw err;
         console.log("1 record inserted");
@@ -132,6 +63,20 @@ app.post('/data', function (req, res) {
         connection.query(SQL, (err, result) => {
             if (err) {
                 throw err;
+            }
+            console.log(result)
+            res.send(JSON.stringify(result))
+        });
+    }); */
+
+    var sql = "INSERT INTO `Person`(`person_id`,`first_name`, `last_name`, `age`) VALUES ('" + req.body.id + "','" + req.body.firstname + "','" + req.body.lastname + "','" + req.body.age + "')";
+    connection.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("1 record inserted");
+        const SQL = 'SELECT * FROM person';
+        connection.query(SQL, (err, result) => {
+            if (err) {
+                res.send(err);
             }
             console.log(result)
             res.send(JSON.stringify(result))
